aca9d06fa5cd12d676af3964c21c292eff93f93a
Marcos Juan Juan <webbegg@gmail.com>
2021-01-05 12:32:01 +0100

    Añadimos NextJS

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Twitter app</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </div>
+)
+
+export default Home
